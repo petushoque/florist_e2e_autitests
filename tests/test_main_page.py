f78b6c63a1ef600133e336bc22ffbd092cd2c0d9
Kubasov Alexei <kubasovalexei@yandex.ru>
2023-06-23 20:50:43 +0700

    Add Base class with select location function

--- tests/test_main_page.py
+++ tests/test_main_page.py
@@ -3,13 +3,16 @@ from pages.main_page import Main_page
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
 
 def test_buy_product():
     s = Service(ChromeDriverManager().install())
-    driver = webdriver.Chrome(service=s)
+    caps = DesiredCapabilities().CHROME
+    caps["pageLoadStrategy"] = "normal"
+    options = webdriver.ChromeOptions()
+    options.add_argument('--ignore-certificate-errors')
+    options.add_argument('--ignore-ssl-errors')
+    driver = webdriver.Chrome(service=s, desired_capabilities=caps, options=options)
     main_page = Main_page(driver)
-    #driver.get('https://www.florist.ru/')
-    #driver.maximize_window()
-    #time.sleep(10)
     main_page.open_main_page()
